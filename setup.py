f835752d2459a654476888a8c1e6494da1586247
Havilash Sivaratnam <havilash2005@gmail.com>
2023-01-27 08:32:27 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io
+import codecs
+import os
+import sys
+
+import pygame_ui
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get("encoding", "utf-8")
+    sep = kwargs.get("sep", "\n")
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(
+    name="pygame-ui",
+    version=pygame_ui.__version__,
+    author="havilash sivaratnam",
+    tests_require=["pytest"],
+    install_requires=["pygame"],
+    cmdclass={"test": PyTest},
+    author_email="havilash2005@gmail.com",
+    description="Pygame UI components",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    test_suite="pygame_ui.test.test_pygame_ui",
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    extras_require={
+        "testing": ["pytest"],
+    },
+)
